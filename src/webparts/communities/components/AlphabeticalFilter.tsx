b7b8682efc1f7abb7239cda5790755605dbd8fb1
gabmor38 <gabriela.morenor@gmail.com>
2024-02-28 17:09:13 -0500

    added the AZ filter for the API

---
+++ src/webparts/communities/components/AlphabeticalFilter.tsx
@@ -0,0 +1,65 @@
+import { IPivotStyles, Pivot, PivotItem } from '@fluentui/react';
+import * as React from 'react';
+import  styles from './Communities.module.scss'
+ 
+
+interface IAlphabeticalFilterProps {
+    selectedLetter: string;
+ 
+}
+
+const AlphabeticalFilter: React.FC<IAlphabeticalFilterProps> = (props) => {
+
+    const { selectedLetter } = props;
+
+    const arrayAtoZ = (): string[] => {
+        const navitems: string[] = [];
+        for (let i = 65; i < 91; i++) {
+          navitems.push(String.fromCharCode(i));
+        }
+        navitems.push(String.fromCharCode(35));
+        return navitems;
+      };
+    
+
+    const combinedIndex = arrayAtoZ();
+
+    const _handleSelectedLetter = (item: PivotItem):void => {
+        console.log("item", item);
+    }
+
+    const pivotStyles: Partial<IPivotStyles> = {
+        link:{
+            backgroundColor:'#e3e1e1',
+        },
+
+        root:{
+            color:'black',
+            marginTop:'20px',
+            marginBottom: '20px',
+        },
+
+        linkIsSelected:{
+            color: 'white'
+        }
+    }
+
+
+    return (
+        <Pivot styles={pivotStyles} className={styles.letter} onLinkClick={_handleSelectedLetter} selectedKey={selectedLetter} linkFormat='tabs'  linkSize='normal' >
+        {combinedIndex.map((letter: string, index: number) => {
+             return (
+               <PivotItem
+                 key={index}
+                 itemKey={letter}
+                 headerText={letter}
+                 headerButtonProps={{'data-title': `${letter}`}}
+               />
+             );
+           })}
+           </Pivot>
+    )
+
+}
+
+export default AlphabeticalFilter;
