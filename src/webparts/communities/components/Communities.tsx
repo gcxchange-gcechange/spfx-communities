d443c9c0f90a7d17d0d61759780c30607a3daae4
gabmor38 <gabriela.morenor@gmail.com>
2024-05-08 10:19:26 -0400

    removed console logs and unused code + aligned title with cards

--- src/webparts/communities/components/Communities.tsx
+++ src/webparts/communities/components/Communities.tsx
@@ -32,36 +32,12 @@ const Communities: React.FC<ICommunitiesProps> = (props) => {
     setCurrentPage(1);
     setIsLoading(false);
   };
-
-  // const _getPageViews = (filteredGroups: any): void => {
-  //   filteredGroups.map((group: any) => {
-  //     GraphService.pageViewsBatch(group.siteId).then((siteViews) => {
-  //       setFilteredGroups((prevFilteredGroups) => {
-  //         const updatedFiltered = prevFilteredGroups.map((groupItems) =>
-  //           groupItems.id === group.id
-  //             ? {
-  //                 ...groupItems,
-  //                 views: siteViews,
-  //               }
-  //             : groupItems
-  //         );
-
-  //         return updatedFiltered;
-  //       });
-  //       setIsLoading(false);
-  //     });
-  //   });
-  // };
-
   
   const _getPageViews = (filteredGroups: any): void => {
-    console.log("filter", filteredGroups)
     const promises = filteredGroups.map((group: any) => {
       return GraphService.pageViewsBatch(group.siteId).then((siteViews) => {
-        console.log("SiteViews", siteViews);
         let siteView: string = '';
         if ( siteViews === undefined) {
-          console.log("view Undefiend")
           siteView = '0'
       } else {
         siteView = siteViews
@@ -80,36 +56,16 @@ const Communities: React.FC<ICommunitiesProps> = (props) => {
     });
   };
 
-
-  // const removeGroupsWithoutURL = (updatedGroups: any): any[] => {
-  //   console.log("remove", updatedGroups);
-  //   // const filterGroup = updatedGroups.filter((group: any) => group.url);
-  //   setFilteredGroups(updatedGroups);
-  //   // _getPageViews(filterGroup);
-  //   return updatedGroups;
-  // };
-
   
   const _getGroupDetailsData = (groups: any): void => {
-    console.log("Getting Details", groups);
     const promises = groups.map((groupData: any) => {
-      console.log("loader", isLoading);
       return GraphService.getGroupDetailsBatch(groupData.id).then((groupDetails) => {
         try {
           if (groupDetails[1] !== undefined) {
-          //  const firstLetter: string[] = groupData.displayName.match(/\b\w/g).slice(0, 2).join("").toUpperCase();
-          //  const firstTwoLetters: string = firstLetter.toString();
-
-           //const encodeUri = encodeURI(firstTwoLetters);
-
-           //console.log("en", encodeUri);
             let thumbnail: string | undefined = undefined;
             if (groupDetails[3] === undefined ) {
-              console.log("thumbnail", groupDetails[3]);
-              //console.log("data:image/jpeg;base64," + encodeUri)
               thumbnail = undefined;
             } else {
-              // console.log("data:image/jpeg;base64," + encodeUri)
               thumbnail = groupDetails[3]
             }
             return {
@@ -139,46 +95,6 @@ const Communities: React.FC<ICommunitiesProps> = (props) => {
   };
 
 
-
-  // const _getGroupDetailsData = (groups: any): void => {
-  //   console.log("Getting Details");
-  //   let groupCount = 0;
-  //   groups.map((groupData: any) => {
-  //     GraphService.getGroupDetailsBatch(groupData.id).then((groupDetails) => {
-  //       try {
-  //         if ( groupDetails[1] !== undefined) 
-  //         {
-  //           setIsLoading(true);
-  //           setGroups((prevGroups) => {
-  //             const updatedGroups = prevGroups.map((groupItems) =>
-  //               groupItems.id === groupData.id
-  //                 ? {
-  //                     ...groupItems,
-  //                     url: groupDetails[1].webUrl,
-  //                     siteId: groupDetails[1].id,
-  //                     modified: new Date( groupDetails[1].lastModifiedDateTime ).toLocaleDateString("en-CA"),
-  //                     members: groupDetails[2],
-  //                     thumbnail: "data:image/jpeg;base64," + groupDetails[3],
-  //                   }
-  //                 : groupItems
-  //             );
-  //             console.log("GC", groupCount);
-  //             removeGroupsWithoutURL(updatedGroups);
-  //             return updatedGroups;
-
-  //           });
-  //         } else {
-  //           console.log(`Group details not found for ${groupData.id}`);
-  //           return null;
-  //         }
-  //       } catch (error) {
-  //         console.log("ERROR", error);
-  //       }
-  //     });
-  //   });
-  // };
-
-
   const _getUserGroups = (): void => {
     GraphService.getUserGroups().then((data) => {
       setGroups(data);
@@ -188,15 +104,12 @@ const Communities: React.FC<ICommunitiesProps> = (props) => {
   };
 
   const _getAllGroups = (selectedLetter: string): void => {
-    console.log("Lo", isLoading);
     GraphService.getAllGroups(selectedLetter).then((allGroupData) => {
-      console.log("GroupData", allGroupData);
       if (allGroupData.responseResults !== undefined) {
         setGroups(allGroupData.responseResults);
         _getGroupDetailsData(allGroupData.responseResults);
       } else {
         setGroups(allGroupData.responseResults)
-        // return null;
       }
 
     });
@@ -204,12 +117,10 @@ const Communities: React.FC<ICommunitiesProps> = (props) => {
 
 
   const getSelectedLetter = (letter: string): void => {
-
     if (letter !== selectedLetter) {
       setSelectedLetter(letter);
       setIsLoading(true);
     }
-  
   };
 
   const openPropertyPane = (): void => {
@@ -217,11 +128,8 @@ const Communities: React.FC<ICommunitiesProps> = (props) => {
   };
 
   const onPageUpdate = (pageNumber: number): void => {
-    console.log("page#",pageNumber)
     setCurrentPage( pageNumber);
-    console.log("current page",currentPage)
-
-  }
+  };
 
 
   useEffect(() => {   
@@ -235,7 +143,6 @@ const Communities: React.FC<ICommunitiesProps> = (props) => {
   }, [props.targetAudience]);
 
   useEffect(() => {
-
     if (targetAudience === "1" && previousLetterRef.current !== selectedLetter) {
       clearState();
       _getAllGroups(selectedLetter);
@@ -291,7 +198,7 @@ const Communities: React.FC<ICommunitiesProps> = (props) => {
             <Spinner size={SpinnerSize.large} />
           ) : (
             <>
-              <h3>{(props.prefLang === "FR" ? props.titleFr : props.titleEn )}</h3>
+              <h3 style={{paddingLeft: '8px'}}>{(props.prefLang === "FR" ? props.titleFr : props.titleEn )}</h3>
               {layout === "Compact" && (
                 <CompactLayoutStyle groups={displayUserGroups} seeAllLink={props.seeAllLink} groupsPerPage={props.numberPerPage} totalGroups={filteredGroups} prefLang={props.prefLang}  createCommLink={props.createCommLink}/>
               )}
